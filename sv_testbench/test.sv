2b79e96a2c728d0c8d08f216eaa5c820ee06f583
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-08-18 18:37:21 +0530

    Create test.sv

---
+++ sv_testbench/test.sv
@@ -0,0 +1,20 @@
+program automatic test(router_io.TB rtr_io);
+
+import router_test_pkg::*;
+
+  Environment env;
+
+  initial begin
+   
+	env = new("env", rtr_io);
+
+	env.configure();
+
+	run_for_n_packets = env.run_for_n_packets;
+
+	
+	env.run();
+	
+  end
+
+endprogram: test
